6a2e6cce9970e586a7370a4a77b05e98d7779ba3
syafinasd <syafinasd@gmail.com>
2024-07-31 12:37:40 +0700

    Update coco_classes.py

--- yolov7/coco_classes.py
+++ yolov7/coco_classes.py
@@ -1,11 +1,15 @@
 COCO_CLASSES = (
-    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
-    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
-    'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
-    'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 
-    'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 
-    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
-    'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 
-    'hair drier', 'toothbrush'
+    'Adem Sari Panas Dalam', 'Ahh Rasa Roti Goguma Korea', 'Azarine Acne Gentle Cleansing Foam',
+    'Azarine Sunscreen Gel', 'Barakat Pasta Gigi', 'Biore Pore Pack with Charcoal', 'Biore UV',
+    'Cleo Air Mineral', 'Club Air Mineral', 'Eiem Beauty Natural Sunscreen', 'Emeron Lovely White UV Jeju Orange',
+    'Emina Bright Stuff Face Wash', 'Emina Sun Protection', 'Energen Rasa Coklat', 'Eternalplus',
+    'Focallure 3 in 1 Palette', 'FreshCare Hot', 'Gimbori Rumput Laut Kering Tabur', 'Hot In Cream',
+    'Implora Jelly Tint', 'Indomie Rasa Kaldu Ayam', 'Indomilk Korean Black Latte', 'Le Minerale',
+    'Makarizo Hair Energy Morning Dew', 'Master Potato Crisps Rendang', 'Milku Susu UHT Rasa Coklat Premium',
+    'Milku Susu UHT Rasa Stroberi', 'Mixagrip Flu - Batuk', 'Nano Nano Milky Stroberi', 'Neo Coffee',
+    'Pop Ice Rasa Anggur', 'Pop Ice Rasa Cokelat', 'Pop Ice Rasa Taro', 'Prochiz Spready Keju Oles',
+    'Promag Herbal', 'Qlife Menstrual Care', 'Richeese Pasta Keju', 'Skintific Sunscreen Mist',
+    'Soffell Wangi Apel', 'Standart Pen AE7', 'Teh Javana', 'Tolakangin', 'Tolakangin Anak',
+    'Ultramilk Rasa Taro', 'Vape Mat 4.1 MV', 'Vit Air Mineral', 'Vital Ear Oil', 'Vitamin B',
+    'Yplogy Hair Mask Gingseng', 'Zen Antibacterial'
 )
